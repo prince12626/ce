3a27c247eea787c1ad360adc5618d7dde89a4db2
Prince Chaurasiya <thelofisongs909@gmail.com>
2026-01-05 17:35:48 +0530

    auth implemented

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useClerk } from "@clerk/clerk-react";
 
-function App() {
-  const [count, setCount] = useState(0)
+function AuthButtons() {
+        const { openSignIn, openSignUp } = useClerk();
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        return (
+                <div>
+                        <button onClick={() => openSignIn({})}>
+                                Sign In (Popup)
+                        </button>
+                        <button onClick={() => openSignUp({})}>
+                                Sign Up (Popup)
+                        </button>
+                </div>
+        );
 }
 
-export default App
+export default AuthButtons;
--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App.jsx';
+import { ClerkProvider } from '@clerk/clerk-react';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+        <StrictMode>
+                <ClerkProvider
+                        publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}
+                >
+                        <App />
+                </ClerkProvider>
+        </StrictMode>
+);
